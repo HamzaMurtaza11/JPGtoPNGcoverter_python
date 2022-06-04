da6f00329955b4db47f23c39f1d58e7b16b0fab4
HAMZA MURTAZA <hamzamurtaza11@yahoo.com>
2022-06-04 17:08:43 +0500

    files added
    pokedex is an input folder containing images in png format and poke2 is an output folder containing converted images (PNG format).

---
+++ JPGtoPNG_coverter_python.py
@@ -0,0 +1,18 @@
+import sys
+import os
+from PIL import Image
+
+input_folder= sys.argv[1]
+output_folder=sys.argv[2]
+
+if not os.path.exists(output_folder):
+  os.makedirs(output_folder)
+  #print(input_folder,output_folder)
+
+for filename in os.listdir(input_folder):
+    img= Image.open(f'{input_folder}{filename}')
+    clean_name= os.path.splitext(filename)[0]
+    img.save(f'{output_folder}{clean_name}.png','png')
+    print('Converted')
+    
+
